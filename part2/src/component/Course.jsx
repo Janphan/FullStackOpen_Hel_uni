d945ba6288d9ca1e91525c4b8ec94e0bdcdba362
Janphan <janphan061@gmail.com>
2024-07-17 14:15:49 +0300

    show total, asm 2.2

--- part2/src/component/Course.jsx
+++ part2/src/component/Course.jsx
@@ -18,11 +18,17 @@ const Part = ({ part }) => (
   </li>
 );
 
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Total of {total} exercises</p>;
+};
+
 const Course = ({ course }) => {
   return (
     <>
       <Header courseName={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
