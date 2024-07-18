3ab6e652223594119113500bb2d6aac4ccd82233
Janphan <janphan061@gmail.com>
2024-07-18 18:53:20 +0300

    update show person asm 2.6

--- part2/simple phonebook/src/App.jsx
+++ part2/simple phonebook/src/App.jsx
@@ -4,12 +4,13 @@ import Person from "./components/Person";
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [showAll, setShowAll] = useState(true);
   const addName = (event) => {
     event.preventDefault();
     console.log("button clicked", event.target);
     const personObject = {
       content: newName,
-      id: String(persons.length + 1)
+      id: String(persons.length + 1),
     };
     setPersons(persons.concat(personObject));
     setNewName("");
@@ -18,20 +19,28 @@ const App = () => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
+      {persons.length > 0 && (
+        <button onClick={toggleShowAll}>
+          {showAll ? "Hide all" : "Show All"}
+        </button>
+      )}
       <ul>
-        {persons.map((person) => (
-          <Person key={person.id} person={person}/>
+        {showAll && persons.map((person) => (
+          <Person key={person.id} person={person} />
         ))}
       </ul>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          Name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
