ba5f91b114857423f0aef08ec28b75c3307b07e0
HongD <janphan061@gmail.com>
2024-12-27 12:42:08 +0200

    add asm 3.5, post method

--- part2/simple phonebook/backend/index.js
+++ part2/simple phonebook/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     {
@@ -60,6 +61,32 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).send({ messsage: `Person id ' {id} 'delete` });
 })
 
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        content: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`) 
