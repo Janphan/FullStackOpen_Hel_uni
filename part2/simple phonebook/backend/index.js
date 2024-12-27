20b5ae4038c2976a93c0e823560004e1a389b0bc
HongD <janphan061@gmail.com>
2024-12-27 11:59:33 +0200

    part 3 asm

--- part2/simple phonebook/backend/index.js
+++ part2/simple phonebook/backend/index.js
@@ -41,6 +41,25 @@ app.get('/info', (request, response) => {
         `)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).send({ error: 'Person not found' });
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).send({ messsage: `Person id ' {id} 'delete` });
+})
+
 const PORT = 3001
 app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+console.log(`Server running on port ${PORT}`) 
