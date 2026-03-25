939587ae282a19d28db7f0029a591cc6efe0383d
Hong <janphan061@gmail.com>
2026-03-25 21:09:58 +0200

    ex3.22: Add ESlint to your application and fix all the warnings

--- part2/simple phonebook/backend/models/persons.js
+++ part2/simple phonebook/backend/models/persons.js
@@ -2,17 +2,12 @@ const mongoose = require('mongoose')
 const dns = require('dns')
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 require('dotenv').config()
-
 mongoose.set('strictQuery', false)
-
 const url = process.env.MONGODB_URI
-
 console.log('connecting to', url)
-
-mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
+mongoose.connect(url, { family: 4 }).then(result => {
+    console.log('connected to MongoDB')
+})
     .catch(error => {
         console.log('error connecting to MongoDB:', error.message)
     })
