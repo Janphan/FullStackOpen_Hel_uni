a3f0951142ab1b77dadb991048df44b7730d585d
Hong <janphan061@gmail.com>
2026-05-13 13:54:15 +0300

    Ex6.8: Change the creation of new anecdotes so that anecdotes are stored in the backend

--- part6/zustand-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/zustand-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,12 +1,14 @@
 import { useAnecdoteActions } from '../store'
+import anecdotesService from '../services/anecdotes'
 export const AnecdoteForm = () => {
   const { addAnecdote } = useAnecdoteActions()
 
-  const addAnecdoteHandler = (e) => {
+  const addAnecdoteHandler = async (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
-    addAnecdote(content)
-    e.target.anecdote.value = ''
+    const newAnecdote = await anecdotesService.createNew(content)
+    addAnecdote(newAnecdote)
+    e.target.reset()
   }
 
   return (
